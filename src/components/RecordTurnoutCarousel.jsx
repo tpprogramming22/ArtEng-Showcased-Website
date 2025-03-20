f7b2188e0d2837bb435f5cc9fd37ce4882baffd3
ted <tpprogrammingcontact@gmail.com>
2025-03-20 13:32:53 +0000

    updated carousel

--- src/components/RecordTurnoutCarousel.jsx
+++ src/components/RecordTurnoutCarousel.jsx
@@ -33,12 +33,12 @@ const RecordTurnoutCarousel = () => {
   ];
 
   // Auto carousel every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % slides.length);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentSlide((prev) => (prev + 1) % slides.length);
+//     }, 5000);
+//     return () => clearInterval(interval);
+//   }, []);
 
   const goToSlide = (index) => {
     setCurrentSlide(index);
