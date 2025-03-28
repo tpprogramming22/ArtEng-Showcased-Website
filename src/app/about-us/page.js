7e36b9e830c7ab6e59e9462095f7eeb74fd6bd02
0x53263 <0x53263@gmail.com>
2025-03-28 19:30:05 +0000

    Update page.js

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -62,16 +62,20 @@ export default function AboutUsPage() {
       {/* Our Story */}
       <section className="py-8 px-4 md:px-8">
         <div className="container mx-auto">
-          <div className="mb-1">
+          <div className="mb-4">
             <SectionHeader 
               title="Our Story" 
               subtitle="How We Started" 
             />
           </div>
     
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-1 items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            <SectionHeader 
+              title="Our Story" 
+              subtitle="How We Started" 
+            />
             <div>
-              <p className="mb-1">
+              <p className="mb-4">
                 We are four individuals brought together by our expertise in our respective fields. ArtEng came from the idea that the diverse worlds of art and engineering could work together to offer innovative solutions in their own unique ways. To find out more about who we are, take a look at who are.
               </p>
             </div>
