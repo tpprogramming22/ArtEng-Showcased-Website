f807ddddd6a3913e8a213bbc2b5d5874f2cc3313
0x53263 <0x53263@gmail.com>
2025-04-23 21:17:44 +0100

    Update page.js

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -15,28 +15,40 @@ export default function AboutUsPage() {
       name: "Adam Snelleksz",
       role: "Marketing Director",
       description: "Adam is a marketing and communications expert with over 25 years of experience, including founding two successful agencies.",
-      imageUrl: "/adam.png"
+      imageUrl: "/adam.png",
+      long1: "Adam has a wide and varied work history in marketing, communication and PR with over 25 years experience in many different roles and organisations. After graduating from university with a marketing degree, Adam moved to London and worked in a busy press office for the National Consumer Council. Following two years in the capital, Adam returned to Birmingham as head of communications for Birmingham City Football Club. The role included hosting and organising manager and player press conferences, dealing with media enquiries and setting up the club's first online TV channel, Blues TV. Adam then stayed in the sports industry when he became head of marketing for Birmingham City Council's sports events department. Responsible for the commercial partnerships, ticket sales, marketing and promotion of world and European championships including the World BMX Championships, European Gymnastics Championships, Diamond League Athletics and many more held at the National Indoor Arena and Birmingham Alexander Stadium.",
+      long2: "After working in sport, Adam took the step of setting up his own company - a video production company called CMA Video. He built the company into a widely recognised video agency which produced TV and cinema adverts for Sea Life Centre, Lego Land, Brooks running shoes and many more. After 12 years of successful trading, Adam sold the business and set up an off-shoot marketing company called CMA Media which specialises in digital marketing, social media and website services. Current clients include Aston University and Birmingham based charity, Help Harry Help Others",
+      long3: ""
     },
     {
       id: 2,
       name: "Stephen Fletcher",
       role: "Sales Director",
       description: "Stephen has over 45 years of experience in logistics, project management, sales, and the arts, with a strong focus on supporting student career transitions.",
-      imageUrl: "/steve.png"
+      imageUrl: "/steve.png",
+      long1: "Stephen’s career spans more than 45 years covering a range of roles including Logistics/Transport management, Project management, Key Account management and Sales Business Development. He has worked on behalf of a range of manufacturing and service suppliers including Metal fabrication, Flexible packaging, Precision Engineering and Commercial Heat Treatment",
+      long2: "Stephen’s experience covers a broad range of industrial sectors including Automotive, Agricultural, Aerospace, General Industrial [including tool making and precision engineering] and Defence across UK and Europe. In addition, Stephen’s vast range of experience also incorporates a deep involvement in the arts and media. A long term involvement in theatre has seen him take on roles of acting, producing and directing in a variety of productions. This has led to a passion for promoting and supporting the theatre and creative media sector including writing, live performance, independent film making and production. Similarly, he has managed, overseen and facilitated creative media driven projects, awareness programmes and initiatives for the NHS and Schools. This has also extended to sales networking and training in the corporate sector.",
+      long3: "Stephen’s experience has led to a passion for supporting the transition of students in the move towards a career environment, allowing them access to as much information as they need to complete their studies, because anything is possible if you are supported by the right people."
     },
     {
       id: 3,
       name: "Joan Smith",
       role: "Business Director",
       description: "Joan is an experienced business advisor who connects companies with expert support to help them grow and succeed.",
-      imageUrl: "/jess-smith.jpg"
+      imageUrl: "/jess-smith.jpg",
+      long1: "With over 20 years of experience in business advice and guidance, Joan brings a wealth of knowledge in all areas of business support. Having worked with hundreds of companies across diverse sectors, Joan has always managed to understand their unique challenges and opportunities that businesses face. Her approach is centred around learning about each business, analysing their needs, and providing tailored solutions to help them achieve their goals.",
+      long2: "As an impartial broker, she specialises in connecting businesses with the right experts and resources, ensuring they receive the guidance needed to thrive. Past experience includes managing members for the Chamber of Commerce and successfully fostering a thriving business community in the Solihull area. This included organising and hosting prestigious business awards evenings - celebrating local success stories, recognising outstanding achievements, and creating opportunities for businesses to showcase their impact on the local, national and global stage. These events not only strengthened connections within the business community but also inspired growth and collaboration.",
+      long3: "Joan’s philosophy is ‘Let’s Make a Difference Together’ and she genuinely believes that every business deserves access to expert advice and guidance in all areas. By working together, any business can thrive, achieving goals with clarity, expertise, and a shared purpose."
     },
     {
       id: 4,
       name: "Wendy Bennet",
       role: "Technical Lead",
       description: "Wendy is a pioneering leader for women in engineering, with groundbreaking achievements in precision casting and leadership roles in multiple industry organizations.",
-      imageUrl: "/wendy.png"
+      imageUrl: "/wendy.png",
+      long1: "Wendy is a trailblazer for women in industry and has a reputation for becoming the first female in a variety of positions across many engineering fields. She worked within the precision investment casting industry for 26 years and was CEO and previous owner of Lost Wax Development Limited. Her achievements in the industry are far ranging and include recognition nationally and internationally. Wendy was the first female to be awarded a Fellowship to the Institute of Cast Metal Engineers (ICME) in 2012 and was also the first female Chair of the Cast Metal Federation (CMF) from 2015 to 2017 and is the current President of the West Midlands branch of the ICME.",
+      long2: "In 2016 she visited Dresden for the International Foundry Forum and represented the UK Foundry Industry and has also been a Government advisor on industrial strategy on behalf of the Metals Council. She was invited to join the Council of “Made in the Midlands” after being recognised as one of the only women within the region who was involved in industry and who owned a precision investment casting facility. Wendy has also been involved in the “Advanced Engineering Cluster” which was set up to promote the use of students and under graduates for technical projects within the manufacturing sector, utilising the latest technologies available within the universities.",
+      long3: ""
     }
   ];
 
@@ -136,7 +148,9 @@ export default function AboutUsPage() {
             <div className="flex flex-col">
               <h2 className="text-2xl font-bold mb-2">{selectedMember.name}</h2>
               <p className="text-lg mb-4">{selectedMember.role}</p>
-              <p className="text-white">{selectedMember.description}</p>
+              <p className="text-black">{selectedMember.long1}</p>
+              <p className="text-black">{selectedMember.long2}</p>
+              <p className="text-black">{selectedMember.long3}</p>
             </div>
           </div>
         </div>
