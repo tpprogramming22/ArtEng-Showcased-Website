d7e44305aae78406fb36a1aa25b6b5a46b77cda0
0x53263 <0x53263@gmail.com>
2025-05-06 22:30:02 +0100

    Update page.js
    changed home image as per adams email

--- src/app/page.js
+++ src/app/page.js
@@ -92,7 +92,7 @@ export default function Home() {
             <div className="lg:col-span-6">
               <div>
                 <Image 
-                  src="/newaboutushome.jpg" 
+                  src="/artinhand.jpg" 
                   alt="ArtEng Team" 
                   width={600} 
                   height={400} 
