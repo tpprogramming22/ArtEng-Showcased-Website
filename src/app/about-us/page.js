3e0b9a18fe4706dec19623fc427a558e9393e753
0x53263 <0x53263@gmail.com>
2025-05-14 23:23:25 +0100

    Update page.js

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -180,7 +180,7 @@ export default function AboutUsPage() {
               <div className="relative w-full md:w-96 h-64 md:h-96 flex-shrink-0">
                 <Image src={selectedMember.imageUrl} alt={selectedMember.name} fill className="object-cover rounded-md" />
               </div>
-              <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 ml-6 relative flex-shrink-0">
+              <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 ml-8 relative flex-shrink-0">
                 <div className="relative w-full md:w-10 h-10 md:h-10 flex-shrink-0">
                   <Image src="linkedin.jpg" alt={selectedMember.name} fill className="object-cover rounded-md" />
                 </div>
