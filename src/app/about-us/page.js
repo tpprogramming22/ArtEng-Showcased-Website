11c26aa54cd87b1948b7cb330466cec4cda19a40
0x53263 <0x53263@gmail.com>
2025-06-25 21:38:43 +0100

    Update page.js

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -135,7 +135,7 @@ export default function AboutUsPage() {
                 </div>
                 <h3 className="text-lg font-bold group-hover:text-arteng-dark transition-colors duration-300">{member.name}</h3>
                 <p className="text-m">{member.role}</p>
-                <p className={`text-m text-center ${member.id === 2 ? 'mb-24' : 'mb-8'}`}>{member.description}</p>
+                <p className={`text-m text-center ${member.id === 2 ? 'mb-24' : 'mb-7'}`}>{member.description}</p>
                 
                 {/* Click indicator - Fixed position at bottom */}
                 <div className="absolute bottom-4 right-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
