4c6632a39ebc9e8051a787e6f33819ad88059f21
0x53263 <0x53263@gmail.com>
2025-06-25 22:02:48 +0100

    Update page.js

--- src/app/partners/page.js
+++ src/app/partners/page.js
@@ -49,7 +49,7 @@ export default function PartnersPage() {
           </div>
           
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-            <div className="relative h-auto w-auto bg-gray-200 rounded-md overflow-hidden mb-16">
+            <div className="relative h-3/4 w-3/4 bg-gray-200 rounded-md overflow-hidden mb-16">
               <Image 
                 src="/partnerspagenew.jpg" 
                 alt="Partnership Opportunities" 
