4ceace9223739c0ca55f5bedf9715984efa0f381
ted <tpprogrammingcontact@gmail.com>
2025-07-01 21:35:31 +0100

    API live changes. Can now see uploaded events

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  images: {
+    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
